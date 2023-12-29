802e7a6a4f85e1b0641ab3db9fb47d48266021be
cristodca (Cristopher D. Chavez) <cristodca.studio@gmail.com>
2023-12-28 22:40:46 -0600

    fundaments

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,216 @@
-import Image from 'next/image'
+"use client"
+import { useEffect, useState } from 'react'
+import { getMoviesFromCinemaAndDate } from './scrap.js'
+import { get } from 'http'
+
+async function getMoviesFromCinemaAndDateAsync(cinema: string, date: string) {
+  const movies = await getMoviesFromCinemaAndDate(cinema, date)
+
+  return movies
+}
 
 export default function Home() {
+  const [movies, setMovies] = useState<any>([])
+  const [selectedMovies, setSelectedMovies] = useState<any>([])
+  const [orderedMovies, setOrderedMovies] = useState<any>([])
+  const [currentFinishTime, setCurrentFinishTime] = useState<any>('00:00')
+  
+  useEffect(() => {
+    async function getMovies() {
+      const movies = await getMoviesFromCinemaAndDateAsync('cinepolis-centro-magno', '01 enero')
+
+      setMovies(movies)
+    }
+
+    getMovies()
+  }, [])
+
+  const addSelectedMovie = (movie: any) => {
+    setSelectedMovies((prevState: any) => (
+      [...prevState, movie]
+    ))
+
+    console.log(selectedMovies)
+  }
+
+  const isMovieSelected = (movie: any) => {
+    return selectedMovies.find((selectedMovie: any) => selectedMovie.Id === movie.Id)
+  }
+
+  function ordenarFuncionesPorHora(funciones: any[]) {
+    // Función de comparación para ordenar por hora de inicio
+    const allFormats: any[] = []
+    
+    funciones.forEach((movieFormats: any) => {
+      movieFormats.Formats.forEach((showtimes: any) => {
+        showtimes.Showtimes.forEach((showtime: any) => {
+          allFormats.push({
+            Time: showtime.Time,
+            Movie: movieFormats.Title,
+            RunTime: movieFormats.RunTime,
+            FinishTime: sumarMinutos(showtime.Time, movieFormats.RunTime)
+          })
+        })
+      })
+    })
+
+    // Función de comparación para ordenar por la propiedad "Time"
+    function compareTime(a: any, b: any) {
+      const timeA = a.Time;
+      const timeB = b.Time;
+
+      if (timeA < timeB) {
+          return -1;
+      }
+      if (timeA > timeB) {
+          return 1;
+      }
+      return 0;
+    }
+
+    // Ordenar el array de películas por la propiedad "Time"
+    allFormats.sort(compareTime);
+
+    console.log(allFormats)
+    return allFormats;
+  }
+
+  function encontrarMejorProgramacion(peliculas: any) {
+    // Clonar el array de películas para evitar modificar el original
+    const cloneMovies = ordenarFuncionesPorHora(peliculas)
+
+
+    setOrderedMovies(cloneMovies)
+
+    // console.log('mejor opcion', obtenerPelisSinCoincidencias(cloneMovies))
+    // console.log('mejor opcion', cloneMovies)
+  }
+
+  // Función para sumar minutos a una hora dada
+  function sumarMinutos(hora: any, minutos: any) {
+    let horaMinutos = hora.split(':');
+    let minutosSumar = minutos.split(' ');
+    let minutes = parseInt(minutosSumar[0]);
+
+    let horaInicial = parseInt(horaMinutos[0]);
+    let minutoInicial = parseInt(horaMinutos[1]);
+
+    if (minutes >= 60) {
+      let horas = Math.floor(minutes / 60);
+      let minutos = minutes % 60;
+      horaInicial += horas;
+      minutoInicial += minutos;
+      if (minutoInicial >= 60) {
+        horaInicial += 1;
+        minutoInicial -= 60;
+      }      
+    } else {
+      minutoInicial += minutes;
+      if (minutoInicial >= 60) {
+        horaInicial += 1;
+        minutoInicial -= 60;
+      }
+    }
+
+    console.log(horaInicial + ':' + minutoInicial);
+    return (horaInicial + ':' + minutoInicial);
+  }
+
+  function obtenerPelisSinCoincidencias(peliculas: any) {
+    // Ordenar las películas por hora de inicio
+    peliculas.sort((a: any, b: any) => (a.Time > b.Time) ? 1 : -1);
+
+    // Array para almacenar las películas sin coincidencias
+    let peliculasSinCoincidencias = [];
+
+    // Iterar sobre las películas y verificar la no coincidencia
+    for (let i = 0; i < peliculas.length - 1; i++) {
+        const peliculaActual = peliculas[i];
+        const peliculaSiguiente = peliculas[i + 1];
+
+        // Obtener la hora de finalización de la película actual y la hora de inicio de la siguiente
+        const finishTimeActual = new Date(`01/01/2000 ${peliculaActual.FinishTime}`);
+        const startTimeSiguiente = new Date(`01/01/2000 ${peliculaSiguiente.Time}`);
+
+        // Verificar si no hay coincidencia en el horario
+        if (finishTimeActual < startTimeSiguiente) {
+            peliculasSinCoincidencias.push(peliculaActual);
+        }
+    }
+
+    // Agregar la última película a la lista sin verificar, ya que no hay siguiente película
+    peliculasSinCoincidencias.push(peliculas[peliculas.length - 1]);
+
+    return peliculasSinCoincidencias;
+  }
+
+  function isWatchingTimeAvailable(time: any) {
+    console.log(time, currentFinishTime)
+
+    if (time > currentFinishTime) {
+      return true
+    }
+
+    return false
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='flex'>
+      <main className='container border p-8'>
+        <div className="flex gap-4 mb-8 max-w-full overflow-x-auto">
+          {
+            movies && movies.length > 0 && movies.map((movie: any) => (
+              <div onClick={() => addSelectedMovie(movie)} className={`border rounded p-4 ${isMovieSelected(movie) ? 'border-green-600' : ''}`} key={movie.Id}>
+                {movie.Title}
+              </div>
+            ))
+          }
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="grid grid-cols-1 gap-8">
+          {selectedMovies && selectedMovies.length > 0 && selectedMovies.map((movie: any) => (
+            <div className='border rounded p-4 flex gap-2' key={movie.Id}>
+              <div>
+                <img src={movie.Poster} alt={movie.Title} />
+              </div>
+              <div className="flex flex-col p-2 gap-2">
+                <h2 className='text-2xl font-bold'>({movie.RunTime}) - {movie.Title}</h2>
+                  {
+                    movie.Formats.map((format: any) => (
+                      <div className='mb-4'>
+                        <p className='mb-2'>{format.Name}</p>
+                        <div className="flex gap-2">
+                          {format.Showtimes.map((showtime: any) => (
+                            <span className='border rounded-full py-1 px-2'>
+                              {showtime.Time}
+                            </span>
+                          ))}
+                        </div>
+                      </div>
+                    ))
+                  }
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <section className='border p-8'>
+        <button className='border' onClick={() => encontrarMejorProgramacion(selectedMovies)}>mostrar ordenadas</button>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        {
+          orderedMovies && orderedMovies.length > 0 && orderedMovies.map((movie: any) => (
+            <div onClick={() => setCurrentFinishTime(movie.FinishTime)} className={`border rounded p-4 flex gap-2 ${isWatchingTimeAvailable(movie.Time) ? 'bg-green-700 text-black' : ''}`} key={movie.Movie}>
+              {movie.Time} - {movie.FinishTime} - {movie.Movie}
+            </div>
+          ))
+        }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <button onClick={() => sumarMinutos('14:50', '150 minutos')}>
+          Sumar minutos
+        </button>
+      </section>
+    </div>
   )
 }
---
+++ app/scrap.js
@@ -0,0 +1,41 @@
+// Ruta al archivo JSON
+const jsonFilePath = './CinepolisTest.json';
+
+export function getJsonInformation() {
+  // Devolver una promesa que resuelve con la información JSON
+  return fetch(jsonFilePath)
+    .then(response => {
+      // Verificar si la solicitud fue exitosa
+      if (!response.ok) {
+        throw new Error(`Error al cargar el archivo JSON (${response.status} ${response.statusText})`);
+      }
+
+      // Parsear la respuesta como JSON y devolver la información
+      return response.json();
+    })
+    .catch(error => {
+      console.error('Error al obtener el archivo JSON:', error);
+      throw error; // Propagar el error para que sea manejado en la función que llama a getJsonInformation
+    });
+}
+
+export const getMoviesFromCinemaAndDate = async (cinema, date) => {
+  try {
+    // Obtener la información JSON usando await
+    const cinepolis = await getJsonInformation();
+
+    // Hacer algo con la información obtenida
+    const cinemaInformation = cinepolis.d.Cinemas.find(c => c.Key === cinema);
+    
+    console.log(cinema, cinemaInformation);
+
+    const dateMovies = cinemaInformation.Dates.find(m => m.ShowtimeDate === date);
+
+    console.log('peliculas del dia', dateMovies)
+
+    return dateMovies.Movies
+  } catch (error) {
+    // Manejar errores aquí si es necesario
+    console.error('Error al obtener información para cinepolis:', error);
+  }
+};
